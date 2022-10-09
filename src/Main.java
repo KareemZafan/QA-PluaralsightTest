20b40ffca1a62920bbe02768a6be36fa45fc444e
KareemZafan <kareemm.zafan@gmail.com>
2022-10-09 19:36:41 +0200

    adding Main class

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> sourceControlTools = new ArrayList<>();
+        sourceControlTools.addAll(List.of("git","mercurial","svn","bazaar"));
+
+        for (String tool:sourceControlTools) {
+            System.out.println(tool);
+        }
+
+    }
+}
